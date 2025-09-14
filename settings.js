a17e7596d8689f7be385ba6b1a4d491c5c03cd41
botekhandw <shadowlegend0099@gmail.com>
2025-09-14 06:33:54 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Nethuwa XMD',
+  author: '‎',
+  botName: "Nethuwa XMD",
+  botOwner: 'Nethuwa XMD', // Your name
+  ownerNumber: '93760497955', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0",
+};
+
+module.exports = settings;
